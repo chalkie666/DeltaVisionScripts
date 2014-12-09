7f25186eb7fbc49cf531d5b2ef6c32236646c924
daniel j. white <dan@chalkie.org.uk>
2014-12-09 11:54:52 +0100

    add GE oss license info to all scripts
    ... that dont have it yet.

--- scripts/SoftworxScriptsOMX/OMX_GenerateWidefield.sh
+++ scripts/SoftworxScriptsOMX/OMX_GenerateWidefield.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 #command file for CreateWF
 #for comments and questions, contact kay.oliver.schink@rr-research.no
+
+# Copyright (C) 2014 GE Healthcare
+#  
+# License: See LICENSE.txt file 
+
+# This script is now obsolete since this functionality is now a menu function in the GE Healthcare SoftWoRx v6 software
+
 # usage - drag and drop a .dv file containing SIM raw data onto the icon of this script to generate a pseudo widefield image.
 export infile=$1
 /usr/local/softWoRx/bin/i386/OMXGenerateWFImage \
